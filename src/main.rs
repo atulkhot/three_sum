af12485189048d6674656a9172bfa24dde934715
atulkhot <atul.khot@gmail.com>
2025-11-05 15:47:06 +0530

    initial commit - three test cases working

---
+++ src/main.rs
@@ -0,0 +1,89 @@
+struct Solution {}
+
+impl Solution {
+    // fn skip_identical_elems(mut i: usize, nums: &Vec<i32>) -> usize {
+    //     let elem = nums[i];
+    //     i = i + 1;
+    //     while i < nums.len() && nums[i] == elem {
+    //         i += 1;
+    //     }
+    //     i
+    // }
+
+    pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut sorted = nums;
+        sorted.sort();
+        let mut result: Vec<Vec<i32>> = vec![];
+        let num_elems = sorted.len();
+        let mut left = 0;
+        let mut i = left + 1;
+        loop {
+            i = i + 1; // invariant - i >= left+2
+            if i >= num_elems {
+                left = left + 1;
+                i = left + 2;
+            }
+            if left + 2 >= num_elems {
+                break;
+            }
+            assert!(i >= left + 2);
+            let sum = sorted[left] + sorted[left + 1] + sorted[i];
+            if sum > 0 {
+                left = left + 1;
+                if left + 2 >= num_elems {
+                    break;
+                }
+                if sorted[left] > 0 && sorted[left + 1] > 0 && sorted[left + 2] > 0 {
+                    break;
+                }
+                i = left + 1;
+                continue;
+            } else if sum < 0 {
+                continue;
+            } else {
+                result.push(vec![sorted[left], sorted[left + 1], sorted[i]]);
+                let elem = sorted[i];
+                let mut duplicate_succ_elems_found = false;
+                while i < num_elems && i+1 < num_elems && sorted[i+1] == elem {
+                    i += 1;
+                    duplicate_succ_elems_found = true;
+                }
+                if i == num_elems-1 && sorted[i] == elem && duplicate_succ_elems_found {
+                    break;
+                }
+                continue;
+            }
+        }
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+
+    #[test]
+    fn test_first() {
+        let result = Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]);
+        let expected = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_no_sum() {
+        let result = Solution::three_sum(vec![0, 1, 1]);
+        assert!(result.is_empty());
+    }
+
+    #[test]
+    fn test_input_all_zeros() {
+        let result = Solution::three_sum(vec![0, 0, 0, 0, 0, 0, 0]);
+        let expected = vec![vec![0, 0, 0]];
+        assert_eq!(result, expected);
+    }
+
+}
+
+fn main() {
+    println!("Hello, world!");
+}
